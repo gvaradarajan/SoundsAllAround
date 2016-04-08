1ffcf6dc93af819de1bfd25353c08100ba5a3fc3
Gautham Varadarajan <gauthamvaradarajan@gmail.com>
2016-04-08 12:07:38 -0400

    fix protocol bug, may come back

--- frontend/components/track.jsx
+++ frontend/components/track.jsx
@@ -4,6 +4,9 @@ var TrackStore = require('../stores/track_store');
 var ApiUtil = require('../util/api_util');
 
 var Track = React.createClass({
+  contextTypes: {
+    router: PropTypes.object.isRequired
+  },
   getInitialState: function () {
     return { track: TrackStore.find(this.props.params.id) };
   },
@@ -14,31 +17,38 @@ var Track = React.createClass({
     this.setState({ track: TrackStore.find(newProps.params.id) });
   },
   componentDidMount: function () {
+    var audio = document.getElementById('audio');
+    // audio.addEventListener('metadataloaded', function (e) {
+    //   var bar = $('.main-track-small-rect')[0];
+    //   var time = e.currentTarget.duration;
+    //   bar.style['transition-duration'] = Math.floor(time).toString() + "s";
+    // });
+    this.audioListenerToken = audio.addEventListener("ended", this.receiveEndOfAudio);
     this.listenerToken = TrackStore.addListener(this._onChange);
     ApiUtil.fetchSingleTrack(this.props.params.id, function () {
-
     });
   },
   componentWillUnmount: function () {
     var audio = document.getElementById('audio');
-    //audio.removeEventListener(this.audioListenerToken);
+    audio.removeEventListener("ended");
     this.listenerToken.remove();
   },
   linkToPlaylist: function (playlist_id) {
     this.context.router.push("/playlists/" + playlist_id);
   },
   receiveEndOfAudio: function (e) {
-    $('.small-rect')[0].style['transition-duration'] = '0s';
-    $('.small-rect').removeClass('ended');
+    $('.main-track-small-rect')[0].style.transitionDuration = '0s';
+    $('.main-track-small-rect').removeClass('ended');
   },
   sendTicker: function (){
     var audio = document.getElementById('audio');
-    $('.move')[0].style['transition-duration'] = Math.floor(audio.duration).toString() + "s";
+    $('.main-track-small-rect')[0].style['transition-duration'] = Math.floor(audio.duration).toString() + "s";
     this.audioListenerToken = audio.addEventListener("ended", this.receiveEndOfAudio);
     if (audio.paused) {
-      // $('.small-rect').addClass('move');
+      // $('.main-track-small-rect').addClass('move');
       audio.play();
-      $('.small-rect').toggleClass('ended');
+      $('.main-track-small-rect').toggleClass('main-ended');
+      // debugger
     }
     else {
       // var left = window.getComputedStyle($('.small-rect')[0]).getPropertyValue('left');
@@ -56,7 +66,7 @@ var Track = React.createClass({
       return <li key={playlist.id}
               onClick={this.linkToPlaylist.bind(this, playlist.id)}>{playlist.title}</li>;
     }.bind(this));
-    var el = track && track.audio && (<audio id="audio" ref="audio_tag" src={track.audio} />);
+    var audio = track && track.audio;
     return (
       <div className="track-show content">
         <header className="track-banner banner">
@@ -70,7 +80,7 @@ var Track = React.createClass({
             <img src={track ? track.image : ""} />
           </div>
         </header>
-        {el}
+        <audio id="audio" ref="audio_tag" src={audio} />
         <h1 className="page-header">Playlists That Include This Track: </h1>
         <ul className="track-playlists group">
           {playlistItems}
